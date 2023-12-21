a7a6ebf21304d544c9884f8919b9b830b03494e8
KotonBads <kotonbads@duck.com>
2023-12-21 15:07:37 +0800

    Add .gitignore and example.config.json files, and update PickerButton function in utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -4,11 +4,12 @@ import "github.com/andlabs/ui"
 
 func PickerButton(window *ui.Window, entry *ui.Entry) ui.Control {
 	button := ui.NewButton("Open")
-	button.OnClicked(func(b *ui.Button) {
-		if filepath := ui.OpenFile(window); filepath != "" {
-			entry.SetText(filepath)
-		}
-	},
+	button.OnClicked(
+		func(b *ui.Button) {
+			if filepath := ui.OpenFile(window); filepath != "" {
+				entry.SetText(filepath)
+			}
+		},
 	)
 
 	return button
